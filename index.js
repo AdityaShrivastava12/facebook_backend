f19d378f609c58e00f337a464dfa4c7eb782fab6
Aditya_Shrivastava <babulakaaditya@gmail.com>
2022-04-19 01:05:13 +0530

    fixing problem

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ app.put('/friends/:sender_id/:reciever_id', async(req,res) => {
   const sender_id = req.params.sender_id;
   const reciever_id = req.params.reciever_id;
   try{
-    const friends = await pool.query(`UPADATE friends SET flag = true WHERE person1 = ${sender_id} AND person2 = ${reciever_id} OR person1 = ${reciever_id} AND person2 = ${sender_id} RETURNING *`);
+    const friends = await pool.query(`UPDATE friends SET flag = true WHERE person1 = ${sender_id} AND person2 = ${reciever_id} OR person1 = ${reciever_id} AND person2 = ${sender_id} RETURNING *`);
     res.send(friends.rows);
   } catch (e) {
     console.log(e);
