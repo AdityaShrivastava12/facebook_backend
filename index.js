b720444f7ba96810fe7d46523b6082b64eee9e51
Aditya_Shrivastava <babulakaaditya@gmail.com>
2022-04-19 00:16:58 +0530

    added accept friend request api

--- index.js
+++ index.js
@@ -181,4 +181,15 @@ app.post('/friends/:sender_id/:reciever_id', async(req,res) => {
   }
 })
 
+// accept a friend request
+app.put('friends/:sender_id/:reciever_id', async(req,res) => {
+  const sender_id = req.params.sender_id;
+  const reciever_id = req.params.reciever_id;
+  try{
+    const friends = await pool.query(`UPADATE friends SET flag = true WHERE (person1 = ${sender_id} AND person2 = ${reciever_id}) AND (person2 = ${sender_id} AND person1 = ${reciever_id}) RETURNING *`);
+    res.send(friends.rows);
+  } catch (e) {
+    res.send(e);
+  }
+})
 pool.connect();
